dfd27e112a81f5bbb55d87b8f8a78b7c6592abae
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2026-04-20 14:14:45 +0200

    Add story input filename support to get_input.

--- src/everybody_codes_py_ft/common.py
+++ src/everybody_codes_py_ft/common.py
@@ -1,6 +1,7 @@
 # pyright: strict
 
 import pathlib as pl
+import re
 import sys
 
 
@@ -12,9 +13,11 @@ def get_input(part: int):
 
     solution_folder = pl.Path(sys.argv[0]).parent.relative_to(pl.Path())
     quest = solution_folder.name
-    year = solution_folder.parent.name
+    event = solution_folder.parent.name
+    if m := re.match(r"^story_0*(\d+)$", event):
+        event = m.group(1)
     return (
-        (input_folder / f"everybody_codes_e{year}_q{quest}_p{part}.txt")
+        (input_folder / f"everybody_codes_e{event}_q{quest}_p{part}.txt")
         .read_text()
         .rstrip()
     )
