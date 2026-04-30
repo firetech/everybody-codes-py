f668eaa36593c1f018f855c896cc689bf3216e46
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2026-04-30 15:54:14 +0200

    Speed up 2025/06 part 3.

--- src/everybody_codes_py_ft/2025/06/mentorship_matrix.py
+++ src/everybody_codes_py_ft/2025/06/mentorship_matrix.py
@@ -24,17 +24,34 @@ def part2():
 
 def part3():
     letters = lib.get_input(3)
-    total_len = len(letters) * 1000
+    input_len = len(letters)
+    repeats = 1000
+    # Fix for handling example
+    while input_len < 2000:
+        # Make sure range doesn't span multiple repeats of the input
+        # (simplifies calculation below)
+        letters *= 10
+        input_len *= 10
+        repeats //= 10
+    assert repeats >= 2, "We're gonna have a problem here"
+    mid_count = repeats - 2
     pairs = 0
     for cat in ("a", "b", "c"):
-        novices = [1 if c == cat else 0 for c in letters] * 1000
-        knights = [1 if c == cat.upper() else 0 for c in letters] * 1000
+        novices = [1 if c == cat else 0 for c in letters] * 3
+        knights = [1 if c == cat.upper() else 0 for c in letters]
         for i, k in enumerate(knights):
             if k == 0:
                 continue
             left = max(0, i - 1000)
-            right = min(i + 1001, total_len)
-            pairs += sum(novices[left:right])
+            right = min(i + 1001, input_len)
+            # Start of arrangement (non-wrapping)
+            pairs += sum(novices[left + input_len : i + input_len + 1001])
+            # Middle repeats of arrangement (wrapping)
+            pairs += (
+                sum(novices[i + input_len - 1000 : i + input_len + 1001]) * mid_count
+            )
+            # End of arrangement (non-wrapping)
+            pairs += sum(novices[i + input_len - 1000 : right + input_len])
 
     print(f"Part 3: {pairs}")
 
