29c7fc46944053e5cb2e690fe7f6d3b71c39d0f0
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2026-05-07 15:32:30 +0200

    Faster solution for 2025/08 part 3.

--- src/everybody_codes_py_ft/2025/08/the_art_of_connection.py
+++ src/everybody_codes_py_ft/2025/08/the_art_of_connection.py
@@ -1,6 +1,7 @@
 # pyright: strict
 
 import itertools as it
+from collections import defaultdict
 
 from everybody_codes_py_ft import common as lib
 
@@ -29,18 +30,33 @@ def part2():
 def part3():
     path = [int(x) for x in lib.get_input(3).split(",")]
     total_nails = max(path)  # Fingers crossed this works...
-    shield = [tuple(sorted(pair)) for pair in it.pairwise(path)]
+    delta = defaultdict[int, int](lambda: 0)
+    strings = defaultdict[int, list[int]](list)
+    for a, b in it.pairwise(path):
+        if a > b:
+            a, b = b, a
+        strings[a].append(b)
+        delta[a + 1] += 1
+        delta[b] -= 1
+
     most_cuts = 0
     for a in range(1, total_nails):
-        for b in range(a + 1, total_nails + 1):
-            cuts = 0
-            for sa, sb in shield:
-                if (
-                    (a > sa and a < sb and b > sb)
-                    or (a < sa and b < sb and b > sa)
-                    or (a == sa and b == sb)
-                ):
-                    cuts += 1
+        # Alter delta for strings starting from this nail.
+        for b in strings[a]:
+            delta[b] += 2  # Collinear if cut goes a->b, change delta from -1 to +1.
+            delta[b + 1] -= 1  # Account for the extra +1 above.
+
+        # Alter delta for strings starting from previous nail.
+        for b in strings[a - 1]:
+            delta[b] -= 1  # Previously collinear, should no longer affect count at b.
+            delta[b + 1] += 2  # Switch from -1 to +1, include in cuts a->(>=b+1).
+
+        cuts = 0
+
+        # Count cuts going from a to >=a+2.
+        # (a->a+1 is not a meaningful cut, and would break the encoding used for delta)
+        for b in range(a + 2, total_nails + 1):
+            cuts += delta[b]
             if cuts > most_cuts:
                 most_cuts = cuts
     print(f"Part 3: {most_cuts}")
