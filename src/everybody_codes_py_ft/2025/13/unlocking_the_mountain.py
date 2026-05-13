6b2ef8c18153de519bb5d6a19eb6568e8c8def32
Joakim Tufvegren <104522+firetech@users.noreply.github.com>
2026-05-13 13:01:16 +0200

    Minor improvements to 2025/13.
    * Simplify wheel building code, no longer needs deque.
    * Since part 2 and 3 need special handling of the input anyway, just
      duplicate the wheel building.
    * Actually use the precalculated segment lengths (`range_lens`) when
      rotating the wheel in part 2 and 3...

--- src/everybody_codes_py_ft/2025/13/unlocking_the_mountain.py
+++ src/everybody_codes_py_ft/2025/13/unlocking_the_mountain.py
@@ -1,51 +1,43 @@
 # pyright: strict
 
 import itertools as it
-from collections import deque
-from typing import TypeVar
 
 from everybody_codes_py_ft import common as lib
 
-T = TypeVar("T")
-
-
-def _make_wheel(initial: T, rest: list[T]):
-    wheel = deque[T]((initial,))
-    for right, *left in it.batched(rest, 2):
-        wheel.append(right)
-        if left:
-            wheel.appendleft(left[0])
-    while wheel[0] != initial:
-        wheel.append(wheel.popleft())
-    return wheel
-
 
 def part1():
-    wheel = _make_wheel(1, [int(line) for line in lib.get_input(1).splitlines()])
+    wheel = [1]
+    lefts = list[int]()
+    for right, *left in it.batched(
+        (int(line) for line in lib.get_input(1).splitlines()), 2
+    ):
+        wheel.append(right)
+        if left:
+            lefts.extend(left)
+    wheel.extend(lefts[::-1])
     print(f"Part 1: {wheel[2025 % len(wheel)]}")
 
 
 def _range_wheel(part: int, moves: int):
-    wheel = _make_wheel(
-        range(1, 2),
-        [
-            (
-                range(int(start), int(end) + 1)
-                if i % 2 == 0
-                else range(int(end), int(start) - 1, -1)
-            )
-            for i, (start, end) in enumerate(
-                line.split("-") for line in lib.get_input(part).splitlines()
-            )
-        ],
-    )
+    wheel = [range(1, 2)]
+    lefts = list[range]()
+    for (right_start, right_end), *left in it.batched(
+        (line.split("-") for line in lib.get_input(part).splitlines()), 2
+    ):
+        wheel.append(range(int(right_start), int(right_end) + 1))
+        if left:
+            left_start, left_end = left[0]
+            lefts.append(range(int(left_end), int(left_start) - 1, -1))
+    wheel.extend(lefts[::-1])
+
     range_lens = [len(r) for r in wheel]
     positions = sum(range_lens)
     end_pos = moves % positions
-    while end_pos >= (pop_len := len(wheel[0])):
-        end_pos -= pop_len
-        wheel.popleft()
-    return wheel[0][end_pos]
+    i = 0
+    while end_pos >= range_lens[i]:
+        end_pos -= range_lens[i]
+        i += 1
+    return wheel[i][end_pos]
 
 
 def part2():
